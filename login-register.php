fa1f23c83b447ca7589ce4396160b1312d49b174
ghani <skghani9999@gmail.com>
2023-08-15 12:52:53 +0530

    first commit

---
+++ login-register.php
@@ -0,0 +1,246 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>OmniFlex || Login-Register</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/all.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/flaticon.css">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/aos.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+</head>
+
+<body>
+
+    <div class="main-wrapper">
+
+<!-- navbar starts -->
+  <?php include "nav.php" ?>
+<!-- navbar ends -->
+
+        <!-- Page Banner Start -->
+        <div class="section page-banner-section" style="background-image: url(assets/images/bg/page-banner.jpg);">
+            <div class="shape-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="944px" height="894px">
+                    <defs>
+                        <linearGradient id="PSgrad_0" x1="88.295%" x2="0%" y1="0%" y2="46.947%">
+                            <stop offset="0%" stop-color="rgb(67,186,255)" stop-opacity="1" />
+                            <stop offset="100%" stop-color="rgb(113,65,177)" stop-opacity="1" />
+                        </linearGradient>
+
+                    </defs>
+                    <path fill-rule="evenodd" fill="rgb(43, 142, 254)" d="M39.612,410.76 L467.344,29.824 C516.51,-13.476 590.638,-9.93 633.938,39.613 L914.192,317.344 C957.492,366.50 953.109,440.637 904.402,483.938 L476.671,864.191 C427.964,907.492 353.376,903.109 310.76,854.402 L29.823,576.670 C-13.477,527.963 -9.94,453.376 39.612,410.76 Z" />
+                    <path fill="url(#PSgrad_0)" d="M39.612,410.76 L467.344,29.824 C516.51,-13.476 590.638,-9.93 633.938,39.613 L914.192,317.344 C957.492,366.50 953.109,440.637 904.402,483.938 L476.671,864.191 C427.964,907.492 353.376,903.109 310.76,854.402 L29.823,576.670 C-13.477,527.963 -9.94,453.376 39.612,410.76 Z" />
+                </svg>
+            </div>
+            <div class="shape-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="726.5px" height="726.5px">
+                    <path fill-rule="evenodd" stroke="rgb(255, 255, 255)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" opacity="0.302" fill="none" d="M28.14,285.269 L325.37,21.217 C358.860,-8.851 410.655,-5.808 440.723,28.14 L704.777,325.36 C734.846,358.859 731.802,410.654 697.979,440.722 L400.955,704.776 C367.132,734.844 315.338,731.802 285.269,697.978 L21.216,400.954 C-8.852,367.132 -5.808,315.337 28.14,285.269 Z" />
+                </svg>
+            </div>
+            <div class="shape-3">
+                <svg xmlns="http://www.w3.org/2000/svg" width="515px" height="515px">
+                    <defs>
+                        <linearGradient id="PSgrad_0" x1="0%" x2="89.879%" y1="0%" y2="43.837%">
+                            <stop offset="0%" stop-color="rgb(67,186,255)" stop-opacity="1" />
+                            <stop offset="100%" stop-color="rgb(113,65,177)" stop-opacity="1" />
+                        </linearGradient>
+
+                    </defs>
+                    <path fill-rule="evenodd" fill="rgb(43, 142, 254)" d="M19.529,202.280 L230.531,14.698 C254.559,-6.661 291.353,-4.498 312.714,19.528 L500.295,230.531 C521.656,254.558 519.493,291.353 495.466,312.713 L284.463,500.295 C260.436,521.655 223.641,519.492 202.281,495.465 L14.699,284.462 C-6.660,260.435 -4.498,223.640 19.529,202.280 Z" />
+                    <path fill="url(#PSgrad_0)" d="M19.529,202.280 L230.531,14.698 C254.559,-6.661 291.353,-4.498 312.714,19.528 L500.295,230.531 C521.656,254.558 519.493,291.353 495.466,312.713 L284.463,500.295 C260.436,521.655 223.641,519.492 202.281,495.465 L14.699,284.462 C-6.660,260.435 -4.498,223.640 19.529,202.280 Z" />
+                </svg>
+            </div>
+            <div class="shape-4">
+                <svg xmlns="http://www.w3.org/2000/svg" width="972.5px" height="970.5px">
+                    <path fill-rule="evenodd" stroke="rgb(255, 255, 255)" stroke-width="1px" stroke-linecap="butt" stroke-linejoin="miter" fill="none" d="M38.245,381.102 L435.258,28.158 C480.467,-12.32 549.697,-7.964 589.888,37.244 L942.832,434.257 C983.23,479.466 978.955,548.697 933.746,588.888 L536.733,941.832 C491.524,982.23 422.293,977.955 382.103,932.745 L29.158,535.732 C-11.32,490.523 -6.963,421.293 38.245,381.102 Z" />
+                </svg>
+            </div>
+            <div class="container">
+                <div class="page-banner-wrap">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <!-- Page Banner Content Start -->
+                            <div class="page-banner text-center">
+                                <h2 class="title">Login & Register</h2>
+                                <ul class="breadcrumb justify-content-center">
+                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Login & Register</li>
+                                </ul>
+                            </div>
+                            <!-- Page Banner Content End -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Banner End -->
+
+        <!-- Login & Register Start -->
+        <div class="section login-register-section section-padding-02">
+            <div class="container">
+
+                <!-- Login & Register Wrapper Start -->
+                <div class="login-register-wrap">
+                    <div class="row gx-5">
+                        <div class="col-lg-6">
+
+                            <!-- Login & Register Box Start -->
+                            <div class="login-register-box">
+                                <!-- Section Title Start -->
+                                <div class="section-title">
+                                    <h2 class="title">Login</h2>
+                                </div>
+                                <!-- Section Title End -->
+
+                                <div class="login-register-form">
+                                    <form action="#">
+                                        <div class="single-form">
+                                            <input type="text" class="form-control" placeholder="Username or email ">
+                                        </div>
+                                        <div class="single-form">
+                                            <input type="password" class="form-control" placeholder="Password">
+                                        </div>
+                                        <div class="single-form form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                                            <label class="form-check-label" for="flexCheckDefault">Remember me</label>
+                                        </div>
+                                        <div class="form-btn">
+                                            <button class="btn">Login</button>
+                                        </div>
+                                        <div class="single-form">
+                                            <p><a href="#">Lost your password?</a></p>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- Login & Register Box End -->
+
+                        </div>
+                        <div class="col-lg-6">
+
+                            <!-- Login & Register Box Start -->
+                            <div class="login-register-box">
+                                <!-- Section Title Start -->
+                                <div class="section-title">
+                                    <h2 class="title">Register</h2>
+                                </div>
+                                <!-- Section Title End -->
+
+                                <div class="login-register-form">
+                                    <form action="#">
+                                        <div class="single-form">
+                                            <input type="text" class="form-control" placeholder="Email ">
+                                        </div>
+                                        <div class="single-form">
+                                            <input type="text" class="form-control" placeholder="Username or email ">
+                                        </div>
+                                        <div class="single-form">
+                                            <input type="password" class="form-control" placeholder="Password ">
+                                        </div>
+                                        <div class="single-form">
+                                            <input type="password" class="form-control" placeholder="Confirm Password ">
+                                        </div>
+                                        <div class="form-btn">
+                                            <button class="btn">Register</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- Login & Register Box End -->
+
+                        </div>
+                    </div>
+                </div>
+                <!-- Login & Register Wrapper End -->
+
+            </div>
+        </div>
+        <!-- Login & Register End -->
+
+        <!-- Cta Start -->
+        <div class="section techwix-cta-section-02 section-padding-02">
+            <div class="container">
+                <!-- Cta Wrap Start -->
+                <div class="cta-wrap" style="background-image: url(assets/images/bg/cta-bg.jpg);">
+                    <div class="row align-items-center">
+                        <div class="col-xl-9 col-lg-8">
+                            <!-- Cta Content Start -->
+                            <div class="cta-content">
+                                <div class="cta-icon">
+                                    <img src="assets/images/cta-icon2.png" alt="">
+                                </div>
+                                <p>We’re Delivering the best customer Experience</p>
+                            </div>
+                            <!-- Cta Content End -->
+                        </div>
+                        <div class="col-xl-3 col-lg-4">
+                            <!-- Cta Button Start -->
+                            <div class="cta-btn">
+                                <a class="btn btn-white" href="#">+44 920 090 505</a>
+                            </div>
+                            <!-- Cta Button End -->
+                        </div>
+                    </div>
+                </div>
+                <!-- Cta Wrap End -->
+            </div>
+        </div>
+        <!-- Cta End -->
+
+<!-- footer section starts -->
+     <?php include "footer.php" ?>
+
+<!-- footer section ends -->
+    </div>
+
+    <!-- JS
+    ============================================ -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="assets/js/plugins/popper.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/aos.js"></script>
+    <script src="assets/js/plugins/waypoints.min.js"></script>
+    <script src="assets/js/plugins/back-to-top.js"></script>
+    <script src="assets/js/plugins/jquery.counterup.min.js"></script>
+    <script src="assets/js/plugins/appear.min.js"></script>
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+
+</html>
---
+++ nav.php
@@ -0,0 +1,118 @@
+  <!-- Preloader start -->
+  <div id="preloader">
+            <div class="preloader">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <!-- Preloader End -->
+
+        <!-- Header Start  -->
+        <div id="header" class="section header-section">
+
+            <div class="container">
+
+                <!-- Header Wrap Start  -->
+                <div class="header-wrap">
+
+                    <div class="header-logo">
+                        <a href="index.php"><img src="" alt=""></a>
+                    </div>
+
+                    <div class="header-menu d-none d-lg-block">
+                        <ul class="main-menu">
+                            <li class="active-menu">
+                                <a href="index.php">Home</a>
+                                <!-- <ul class="sub-menu">
+                                    <li class="active"><a href="index.">Home Main</a></li>
+                                  
+                                </ul> -->
+                            </li>
+                            <li>
+                                <a href="about.php">Aboute Us</a>
+                            </li>
+                            <li><a href="service.php">Services</a>
+                            
+                            </li>
+                       
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- Header Meta Start -->
+                    <div class="header-meta">
+                        <!-- Header Cart Start -->
+                      
+                        <!-- Header Cart End -->
+                        <!-- Header Search Start -->
+                        <div class="header-search">
+                            <a class="search-btn" href="#"><i class="flaticon-loupe"></i></a>
+                            <div class="search-wrap">
+                                <div class="search-inner">
+                                    <i id="search-close" class="flaticon-close search-close"></i>
+                                    <div class="search-cell">
+                                        <form action="#">
+                                            <div class="search-field-holder">
+                                                <input class="main-search-input" type="search" placeholder="Search Your Keyword...">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Header Search End -->
+
+                        <div class="header-btn d-none d-xl-block">
+                            <a class="btn" href="login-register.php">Get Started</a>
+                        </div>
+                        <!-- Header Toggle Start -->
+                        <div class="header-toggle d-lg-none">
+                            <button data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                        <!-- Header Toggle End -->
+                    </div>
+                    <!-- Header Meta End  -->
+
+                </div>
+                <!-- Header Wrap End  -->
+
+            </div>
+        </div>
+        <!-- Header End -->
+        <!-- Offcanvas Start-->
+        <div class="offcanvas offcanvas-start" id="offcanvasExample">
+            <div class="offcanvas-header">
+                <!-- Offcanvas Logo Start -->
+                <div class="offcanvas-logo">
+                    <a href="index.php"><img src="assets/images/logo-white.png" alt=""></a>
+                </div>
+                <!-- Offcanvas Logo End -->
+                <button type="button" class="close-btn" data-bs-dismiss="offcanvas"><i class="flaticon-close"></i></button>
+            </div>
+
+            <!-- Offcanvas Body Start -->
+            <div class="offcanvas-body">
+                <div class="offcanvas-menu">
+                    <ul class="main-menu">
+                        <li>
+                            <a href="index.php">Home</a>
+                          
+                        </li>
+                        <li class="active-menu">
+                            <a href="about.php">Aboute Us</a>
+                        </li>
+                        <li><a href="service.php">Servces</a>
+                        
+                        </li>
+    
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Offcanvas Body End -->
+        </div>
+        <!-- Offcanvas End -->
